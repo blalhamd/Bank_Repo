2e69e580d7d8c4bfd0f8d72a68d5c7cde8613dca
Bilal <blalhamd48@gmail.com>
2024-07-05 04:19:35 +0300

    add des 3

--- Entities/Bank.cs
+++ Entities/Bank.cs
@@ -6,7 +6,7 @@ namespace BankSystem.Entities
         public int Id { get; set; } // PK
         public string NameBank { get; set; } // following Naming rules
         public string phone { get; set; }
-        public string address { get; set; }
+        public string address { get; set; } // may be composite
         public float NumberBranches { get; set; } = 0f; // "f" for data type float
         public ICollection<Account> accounts { get; set; } = new List<Account>();
         public ICollection<Customer> customers { get; set; } = new List<Customer>();
